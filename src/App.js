0a328678ff793501d5e9ed9b8e6441b63726b359
mwturner611 <mwturner611@gmail.com>
2020-06-28 15:33:49 -0500

    links open new tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './App.css';
 import turner from "./Images/Turner.jpg";
+import bootstrap from "./Images/Bootstrap.png";
+import react from "./Images/react.png";
+import mysql from "./Images/mySQL.png";
+import mongodb from "./Images/mongodb.png";
+import mongoose from "./Images/mongoose.png";
+import howsitgrowing from "./Images/howsitgrowing.JPG";
+import weather from "./Images/weather.JPG";
+import burger from "./Images/burger.JPG";
+import demo from "./Images/demo.gif";
+import mars from "./Images/mars.JPG";
+import note from "./Images/NoteTaker.JPG";
+
 
 function App() {
   return (
@@ -14,10 +26,10 @@ function App() {
             </a>
           
             <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <a className="dropdown-item" href="mailto:mwturner611@gmail.com">Email: mwturner611@gmail.com</a>
-              <a className="dropdown-item" href="https://www.linkedin.com/in/matt-turner-ba328211a/">LinkedIn</a>
-              <a className="dropdown-item" href="https://github.com/mwturner611">Github</a>
-              <a className="dropdown-item" href="https://drive.google.com/file/d/1QCiuGbfZzCHkoTmAaynAgrOwAaxXnOlQ/view?usp=sharing">Resume</a>
+              <a className="dropdown-item" target="_blank" href="mailto:mwturner611@gmail.com">Email: mwturner611@gmail.com</a>
+              <a className="dropdown-item" target="_blank" href="https://www.linkedin.com/in/matt-turner-ba328211a/">LinkedIn</a>
+              <a className="dropdown-item" target="_blank" href="https://github.com/mwturner611">Github</a>
+              <a className="dropdown-item" target="_blank" href="https://drive.google.com/file/d/1QCiuGbfZzCHkoTmAaynAgrOwAaxXnOlQ/view?usp=sharing">Resume</a>
             </div>
         </div>
 
@@ -66,7 +78,7 @@ function App() {
                                 <blockquote className="blockquote mb-0">
                                     <p>I like to work with code with the people it's benefiting in mind. I'm a web developer who's driven to solve the problem for the end user. I seek to bring clarity and resolution with my development.</p>
                                 </blockquote>
-                                <a className="littleIcon" href="mailto:mwturner611@gmail.com">Email</a> <a className="littleIcon" href="https://www.linkedin.com/in/matt-turner-ba328211a/">LinkedIn</a> <a className="littleIcon" href="https://github.com/mwturner611"><i className="fa fa-github fa-2x"></i></a> <a className="littleIcon" href="https://drive.google.com/file/d/1QCiuGbfZzCHkoTmAaynAgrOwAaxXnOlQ/view?usp=sharing">Resume</a>
+                                <a className="littleIcon" target="_blank" href="mailto:mwturner611@gmail.com">Email</a> <a className="littleIcon" target="_blank" href="https://www.linkedin.com/in/matt-turner-ba328211a/">LinkedIn</a> <a className="littleIcon" target="_blank" href="https://github.com/mwturner611"><i className="fa fa-github fa-2x"></i></a> <a className="littleIcon" target="_blank" href="https://drive.google.com/file/d/1QCiuGbfZzCHkoTmAaynAgrOwAaxXnOlQ/view?usp=sharing">Resume</a>
                             </div>
                             
                             
@@ -91,7 +103,8 @@ function App() {
                             <ul className="list-group list-group-flush">
                               <li className="list-group-item"><i className="fab fa-html5 fa-1x"></i> HTML</li>
                               <li className="list-group-item"><i className="fab fa-css3 fa-1x"></i> CSS</li>
-                              <li className="list-group-item"><img src="Images/Bootstrap.png" height="14px" width="14px" alt="Bootstrap"/> Bootstrap</li>
+                              <li className="list-group-item"><img src={react} height="22px" width="22px" alt="React.js"/> React.js</li>
+                              <li className="list-group-item"><img src={bootstrap} height="14px" width="14px" alt="Bootstrap"/> Bootstrap</li>
                               <li className="list-group-item"><i className="fab fa-js fa-1x"></i> JavaScript</li>
                               <li className="list-group-item">JQuery</li>
                             </ul>
@@ -105,9 +118,9 @@ function App() {
                             <ul className="list-group list-group-flush">
                                 <li className="list-group-item"><i className="fab fa-node-js fa-1x"></i> NodeJS</li>
                                 <li className="list-group-item">Express <i className="fab fa-js fa-1x"></i></li>
-                                <li className="list-group-item"><img src="Images/mySQL.png" height="14px" width="14px" alt="MySQL"/> MySQL</li>
-                                <li className="list-group-item"><img src="Images/mongodb.png" height="14px" width="14px" alt="MongoDB"/> MongoDB</li>
-                                <li className="list-group-item"><img src="Images/mongoose.png" height="20px" width="20px" alt="Mongoose"/>  Mongoose</li>
+                                <li className="list-group-item"><img src={mysql} height="14px" width="14px" alt="MySQL"/> MySQL</li>
+                                <li className="list-group-item"><img src={mongodb} height="14px" width="14px" alt="MongoDB"/> MongoDB</li>
+                                <li className="list-group-item"><img src={mongoose} height="20px" width="20px" alt="Mongoose"/>  Mongoose</li>
                             </ul>
                         </div>
                     </div>
@@ -139,9 +152,9 @@ function App() {
                 <div className="row justify-content-center pgHeader colorText"><h2>Projects</h2></div>
                 <div className="row align-items-center justify-content-center">
                 <div className="col-md-4 topMarg">  
-                    <a href="https://github.com/mwturner611/Hows_It_Growing">  
+                    <a target="_blank" href="https://hows-it-growing.herokuapp.com/">  
                     <div className="card imgfade" style={{width: '18rem;'}}>
-                        <img className="card-img-top img"  src="Images/howsitgrowing.JPG" alt="Card image cap"/>
+                        <img className="card-img-top img"  src={howsitgrowing} alt="Card image cap"/>
                         <div className="middle">
                         <div>Please click to see more</div>
                         </div>
@@ -149,16 +162,16 @@ function App() {
                         <p className="card-text text-center">This app allows you to give your plants the TLC they deserve (and not die)</p>
                         </div>
                         <div className="card-body">
-                            <a href="https://github.com/mwturner611/Hows_It_Growing" className="card-link"><i className="fa fa-github fa-2x"></i></a>
-                            <a href="https://hows-it-growing.herokuapp.com/" className="card-link">Live Link</a>
+                            <a target="_blank" href="https://github.com/mwturner611/Hows_It_Growing" className="card-link"><i className="fa fa-github fa-2x"></i></a>
+                            <a target="_blank" href="https://hows-it-growing.herokuapp.com/" className="card-link">Live Link</a>
                         </div>
                     </div>
                     </a> 
                 </div> 
                     <div className="col-md-4 topMarg">    
-                        <a href="https://mwturner611.github.io/Weather-Dashboard/">
+                        <a target="_blank" href="https://mwturner611.github.io/Weather-Dashboard/">
                         <div className="card imgfade" style={{width: '18rem;'}}>
-                        <img className="card-img-top img" src="Images/weather.JPG" alt="Card image cap"/>
+                        <img className="card-img-top img" src={weather} alt="Card image cap"/>
                         <div className="middle">
                             <div>Please click to see more</div>
                         </div>
@@ -166,17 +179,17 @@ function App() {
                             <p className="card-text text-center">This page allows you to search the weather by city</p>
                         </div>
                         <div className="card-body">
-                            <a href="https://github.com/mwturner611/Weather-Dashboard" className="card-link"><i className="fa fa-github fa-2x"></i></a>
-                            <a href="https://mwturner611.github.io/Weather-Dashboard/" className="card-link">Live Link</a>
+                            <a target="_blank" href="https://github.com/mwturner611/Weather-Dashboard" className="card-link"><i className="fa fa-github fa-2x"></i></a>
+                            <a target="_blank" href="https://mwturner611.github.io/Weather-Dashboard/" className="card-link">Live Link</a>
                         </div>
                         </div>
                         </a>
                     </div>
 
                     <div className="col-md-4 topMarg">
-                        <a href="https://burger-quest.herokuapp.com/">
+                        <a target="_blank" href="https://burger-quest.herokuapp.com/">
                         <div className="card imgfade" style={{width: '18rem;'}}>
-                        <img className="card-img-top img" src="Images/burger.JPG" alt="Card image cap"/>
+                        <img className="card-img-top img" src={burger} alt="Card image cap"/>
                         <div className="middle">
                             <div>Please click to see more</div>
                         </div>
@@ -184,8 +197,8 @@ function App() {
                             <p className="card-text text-center">This app helps you track burgers you want to eat and mark them off as they're eaten.</p>
                         </div>
                         <div className="card-body">
-                            <a href="https://github.com/mwturner611/Burger_Quest" className="card-link"><i className="fa fa-github fa-2x"></i></a>
-                            <a href="https://burger-quest.herokuapp.com/" className="card-link">Live Link</a>
+                            <a target="_blank" href="https://github.com/mwturner611/Burger_Quest" className="card-link"><i className="fa fa-github fa-2x"></i></a>
+                            <a target="_blank" href="https://burger-quest.herokuapp.com/" className="card-link">Live Link</a>
                         </div>          
                         </div>
                         </a>
@@ -194,9 +207,9 @@ function App() {
 
                 <div className="row align-items-center justify-content-center">
                     <div className="col-md-4 topMarg">
-                        <a href="https://github.com/mwturner611/Employee_Tracker">
+                        <a target="_blank" href="https://github.com/mwturner611/Employee_Tracker">
                         <div className="card imgfade" style={{width: '18rem;'}}>
-                            <img className="card-img-top img" src="Images/demo.gif" alt="Card image cap"/>
+                            <img className="card-img-top img" src={demo} alt="Card image cap"/>
                             <div className="middle">
                             <div>Please click to see more</div>
                             </div>
@@ -204,15 +217,15 @@ function App() {
                             <p className="card-text text-center">A simple command line application to draw employee info from a MySQL database</p>
                             </div>
                             <div className="card-body">
-                                <a href="https://github.com/mwturner611/Employee_Tracker" className="card-link"><i className="fa fa-github fa-2x"></i></a>
+                                <a target="_blank" href="https://github.com/mwturner611/Employee_Tracker" className="card-link"><i className="fa fa-github fa-2x"></i></a>
                             </div>  
                         </div>
                         </a>
                     </div>
                     <div className="col-md-4 topMarg">  
-                        <a href="https://mwturner611.github.io/Life_On_Mars/">  
+                        <a target="_blank" href="https://mwturner611.github.io/Life_On_Mars/">  
                         <div className="card imgfade" style={{width: '18rem;'}}>
-                            <img className="card-img-top img" src="Images/mars.JPG" alt="Card image cap"/>
+                            <img className="card-img-top img" src={mars} alt="Card image cap"/>
                             <div className="middle">
                             <div>Please click to see more</div>
                             </div>
@@ -220,16 +233,16 @@ function App() {
                             <p className="card-text text-center">This is a site on Mars calling on two Nasa API's.</p>
                             </div>
                             <div className="card-body">
-                                <a href="https://github.com/mwturner611/Life_On_Mars" className="card-link"><i className="fa fa-github fa-2x"></i></a>
-                                <a href="https://mwturner611.github.io/Life_On_Mars/" className="card-link">Live Link</a>
+                                <a target="_blank" href="https://github.com/mwturner611/Life_On_Mars" className="card-link"><i className="fa fa-github fa-2x"></i></a>
+                                <a target="_blank" href="https://mwturner611.github.io/Life_On_Mars/" className="card-link">Live Link</a>
                             </div>     
                         </div>
                         </a>
                     </div>
                     <div className="col-md-4 topMarg">
-                        <a href="https://note-taker-611.herokuapp.com/">
+                        <a target="_blank" href="https://note-taker-611.herokuapp.com/">
                             <div className="card imgfade" style={{width: '18rem;'}}>
-                                <img className="card-img-top img" src="Images/NoteTaker.JPG" alt="Note Taker"/>
+                                <img className="card-img-top img" src={note} alt="Note Taker"/>
                                 <div className="middle">
                                     <div>Please click to see more</div>
                                 </div>
@@ -237,8 +250,8 @@ function App() {
                                     <p className="card-text text-center">This application uses express to save and retrieve note data from a JSON file</p>
                                 </div>
                                 <div className="card-body">
-                                    <a href="https://github.com/mwturner611/Note_Taker" className="card-link"><i className="fa fa-github fa-2x"></i></a>
-                                    <a href="https://note-taker-611.herokuapp.com/" className="card-link">Live Link</a>
+                                    <a target="_blank" href="https://github.com/mwturner611/Note_Taker" className="card-link"><i className="fa fa-github fa-2x"></i></a>
+                                    <a target="_blank" href="https://note-taker-611.herokuapp.com/" className="card-link">Live Link</a>
                                 </div>   
                             </div>
                         </a>
