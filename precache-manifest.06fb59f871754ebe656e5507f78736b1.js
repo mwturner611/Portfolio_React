4e8ca5e99176eacf14a83e2b73f2f34684a25a72
mwturner611 <mwturner611@gmail.com>
2020-07-01 21:06:25 -0500

    Updates

---
+++ precache-manifest.06fb59f871754ebe656e5507f78736b1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19c553bfd33a2090f8b1dc0c86b2564",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "377d1fec8a183e8bdffa",
+    "url": "/portfolio_react/static/css/main.7c312d9d.chunk.css"
+  },
+  {
+    "revision": "0515b9f52209f55b026d",
+    "url": "/portfolio_react/static/js/2.c080b32d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_react/static/js/2.c080b32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377d1fec8a183e8bdffa",
+    "url": "/portfolio_react/static/js/main.8380d561.chunk.js"
+  },
+  {
+    "revision": "6aecdd57f541cfb08b5a",
+    "url": "/portfolio_react/static/js/runtime-main.fa04381e.js"
+  },
+  {
+    "revision": "5a47a97d885f4c300302201147a68d4b",
+    "url": "/portfolio_react/static/media/NoteTaker.5a47a97d.JPG"
+  },
+  {
+    "revision": "f3c29774d44955f0c9d94009b83a57e2",
+    "url": "/portfolio_react/static/media/Turner.f3c29774.jpg"
+  },
+  {
+    "revision": "f2bf6903a905889d54f8c3cf6afc0799",
+    "url": "/portfolio_react/static/media/burger.f2bf6903.JPG"
+  },
+  {
+    "revision": "b4cc593cdc8bfffca2e9c0cc7105ee35",
+    "url": "/portfolio_react/static/media/demo.b4cc593c.gif"
+  },
+  {
+    "revision": "179047f0f8c80521314f7787f3925d29",
+    "url": "/portfolio_react/static/media/howsitgrowing.179047f0.JPG"
+  },
+  {
+    "revision": "f53c0e25403af2043fc4b3ba644756c8",
+    "url": "/portfolio_react/static/media/mars.f53c0e25.JPG"
+  },
+  {
+    "revision": "5821aa1443889c214400f2458f695ed9",
+    "url": "/portfolio_react/static/media/mongodb.5821aa14.png"
+  },
+  {
+    "revision": "af25e683b9a66ddf279939e77d47ad3a",
+    "url": "/portfolio_react/static/media/mySQL.af25e683.png"
+  },
+  {
+    "revision": "7c0fdc490914ad5e54763db4ef955066",
+    "url": "/portfolio_react/static/media/react.7c0fdc49.png"
+  },
+  {
+    "revision": "deea3d7d6916dd3fbe14d07d04b4b11a",
+    "url": "/portfolio_react/static/media/weather.deea3d7d.JPG"
+  }
+]);
